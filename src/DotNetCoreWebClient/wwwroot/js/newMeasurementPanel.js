60f4b1d9fcef2f833d1d111440f8c645f4e55371
WojtekKempa <kempa.wojtek@gmail.com>
2019-04-16 19:02:47 +0200

    added controller for new measurement panel

---
+++ src/DotNetCoreWebClient/wwwroot/js/newMeasurementPanel.js
@@ -0,0 +1,29 @@
+﻿class NewMeasurementPanel {
+    _listeners = []
+
+    initialize() {
+        this._initAddBtn()
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener)
+    }
+
+    _initAddBtn() {
+        let addBtn = document.getElementById('measurement_addBtn')
+        addBtn.addEventListener('click', e => {
+            let nameInput = document.getElementById('measurement_nameInput')
+            let valueInput = document.getElementById('measurement_valueInput')
+            this._raiseNewMeasurementEvent({
+                name: nameInput.value,
+                value: valueInput.value
+            })
+        })
+    }
+
+    _raiseNewMeasurementEvent(e) {
+        for (let i in this._listeners) {
+            this._listeners[i].newMeasurementAdded(e)
+        }
+    }
+}
