3d8f59a48b249ba249b54d14989839bbd83b2d0d
WojtekKempa <kempa.wojtek@gmail.com>
2019-04-16 19:11:56 +0200

    updated controller for new measurement panel - selectors used instead of getElementById method

--- src/DotNetCoreWebClient/wwwroot/js/newMeasurementPanel.js
+++ src/DotNetCoreWebClient/wwwroot/js/newMeasurementPanel.js
@@ -12,8 +12,8 @@
     _initAddBtn() {
         let addBtn = document.getElementById('measurement_addBtn')
         addBtn.addEventListener('click', e => {
-            let nameInput = document.getElementById('measurement_nameInput')
-            let valueInput = document.getElementById('measurement_valueInput')
+            let nameInput = document.querySelector('#measurement_nameInput')
+            let valueInput = document.querySelector('#measurement_valueInput')
             this._raiseNewMeasurementEvent({
                 name: nameInput.value,
                 value: valueInput.value
