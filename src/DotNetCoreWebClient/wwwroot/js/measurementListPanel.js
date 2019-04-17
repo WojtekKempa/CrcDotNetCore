2ab375d6faa77eddbb4fb9b769a0e080e4046c23
WojtekKempa <kempa.wojtek@gmail.com>
2019-04-17 13:28:21 +0200

    added event listener for removing measurement entries

--- src/DotNetCoreWebClient/wwwroot/js/measurementListPanel.js
+++ src/DotNetCoreWebClient/wwwroot/js/measurementListPanel.js
@@ -1,4 +1,6 @@
 ﻿class MeasurementListPanel {
+    _listeners = []
+
     addNewMeasurement(measurement) {
         let grid = document.querySelector('#measurement_container')
         let newRow = grid.children[1].cloneNode(true)
@@ -10,12 +12,27 @@
         valueColumn.innerText = measurement.value
 
         let removeAnchor = newRow.querySelector('a[data-action=\'remove\']')
-        removeAnchor.addEventListener('click', e => {
+        removeAnchor.addEventListener('click', e => {            
+            let idColumn = removeAnchor.parentElement.parentElement.querySelector("div[data-column-type=\'id\']")
+            this._raiseMeasurementRemovedEvent({
+                id: idColumn.innerText
+            })
+
             removeAnchor.parentElement.parentElement.remove()
         })
 
         newRow.classList.remove('d-none')
 
         grid.appendChild(newRow)
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener);
+    }
+
+    _raiseMeasurementRemovedEvent(e) {
+        this._listeners.forEach(i => {
+            i.measurementRemoved(e)
+        })
     }
 }
