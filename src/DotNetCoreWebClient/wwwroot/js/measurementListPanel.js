386821c0fc4e749048a0cbc983c8a36f6e4adf59
WojtekKempa <kempa.wojtek@gmail.com>
2019-04-17 10:47:59 +0200

    added controller for measurement list panel

---
+++ src/DotNetCoreWebClient/wwwroot/js/measurementListPanel.js
@@ -0,0 +1,16 @@
+﻿class MeasurementListPanel {
+
+    addNewMeasurement(measurement) {
+        let grid = document.querySelector("#measurement_container")
+        let newRow = grid.children[1].cloneNode(true)        
+
+        let removeAnchor = row.querySelector('a[data-action=\'remove\']')
+        removeAnchor.addEventListener('click', e => {
+            removeAnchor.parentElement.parentElement.remove()
+        })
+
+        newRow.classList.remove('d-none')
+
+        grid.appendChild(newRow)
+    }
+}
