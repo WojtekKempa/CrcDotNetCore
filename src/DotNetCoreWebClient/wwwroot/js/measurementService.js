af4d85ef13a15debe5066c3d8079f9f75475a326
WojtekKempa <kempa.wojtek@gmail.com>
2019-05-08 17:32:07 +0200

    added service class for client

---
+++ src/DotNetCoreWebClient/wwwroot/js/measurementService.js
@@ -0,0 +1,32 @@
+﻿class MeasurementService {
+    _serviceUrl = ''
+    _listeners = []
+
+    constructor(serviceUrl) {
+        this._serviceUrl = serviceUrl + 'api/measurement'
+    }
+
+    addEventListener(listener) {
+        this._listeners.push(listener)
+    }
+
+    get() {
+        let req = new XMLHttpRequest()
+        req.onreadystatechange = e => {
+            if (req.readyState == 4 && req.status == 200) {
+                this._raiseGetResponseReady({
+                    data: req.response
+                })
+            }
+        }
+
+        req.open('GET', this._serviceUrl)
+        req.send()
+    }
+
+    _raiseGetResponseReady(e) {
+        this._listeners.forEach(l => {
+            l.getResponseReady(e)
+        })
+    }
+}
